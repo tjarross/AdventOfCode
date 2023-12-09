2ec5954cbdf3cdc5edef73d18117f31b5fe81fc2
tjarross <tjarross@student.42.fr>
2023-12-09 16:14:28 +0100

    Add: Day 9 Solutions

---
+++ 2023/09/j09_1.py
@@ -0,0 +1,12 @@
+def extrapolate_list(values):
+    subsequences = [values]
+    while set(subsequences[-1]) != {0}:
+        subsequences.append([v2 - v1 for v1, v2 in zip(subsequences[-1][:-1], subsequences[-1][1:])])
+    return sum(int(subsequence[-1]) for subsequence in subsequences)
+
+def main():
+    with open("input.txt") as file:
+        print(sum(extrapolate_list(list(map(int, line.split()))) for line in file.read().split("\n")[:-1]))
+
+if __name__ == "__main__":
+    main()
