d6e1f36094eb317b003fe60ae44cc16cdec4a7f3
tjarross <github@thojar.fr>
2023-12-18 16:54:57 +0100

    Add: Day 12 Part 2 (Not Yet Finished)

--- 2023/12/j12_2.py
+++ 2023/12/j12_2.py
@@ -1,14 +1,12 @@
-import datetime
-import time
-
 List = [
-    # "???.### 1,1,3",
-    # ".??..??...?##. 1,1,3",
-    # "?#?#?#?#?#?#?#? 1,3,1,6",
-    # "????.#...#... 4,1,1",
-    # "????.######..#####. 1,6,5",
-    # "?###???????? 3,2,1",
-    "????#????.???????????????#????.?????????? 1,4,2,1,7,1,4,2,1,7"
+    "???.### 1,1,3",
+    ".??..??...?##. 1,1,3",
+    "?#?#?#?#?#?#?#? 1,3,1,6",
+    "????.#...#... 4,1,1",
+    "????.######..#####. 1,6,5",
+    "?###???????? 3,2,1",
+    # "?#????#?..#. 1,2,1",
+    # "????#????.?????????? 1,4,2,1,7",
 ]
 
 
@@ -16,11 +14,70 @@ def count_groups(springs: str):
     return [len(word) for word in springs.split(".") if len(word) != 0]
 
 
-def guess(springs, damaged_groups):
+def try_pattern(pattern: str, springs: str):
+    """
+    >>> try_pattern("#", "??")
+    True
+    >>> try_pattern("#", "???.###")
+    True
+    >>> try_pattern("#", "?")
+    True
+    >>> try_pattern("#####", "?#??#.")
+    True
+    >>> try_pattern("#####", "?#.?#.")
+    False
+    >>> try_pattern("####", "?#??##..??.")
+    False
+    >>> try_pattern("####", "?#??.#..??.")
+    True
+    >>> try_pattern("####", "?#???#..??.")
+    True
+    >>> try_pattern("####", ".#???#..??.")
+    False
+    >>> try_pattern("####", "####..??.")
+    True
+    >>> try_pattern("###", "###?????")
+    True
+    """
+    # print("--->", pattern, springs, end=" ")
+    if len(pattern) > len(springs):
+        # print("NOK")
+        return False
+    if len(pattern) == len(springs):
+        if springs.count('.') != 0:
+            # print("NOK")
+            return False
+        # print("OK")
+        return True
+    if '.' in springs[:len(pattern)] or springs[len(pattern)] == '#':
+        # print("NOK")
+        return False
+    # print("OK")
+    return True
+
+def guess(springs: str, damaged_groups: list):
+    # print(springs, damaged_groups)
+    if len(springs) == 0 and len(damaged_groups) != 0:
+        return 0
+    if len(damaged_groups) == 0:
+        return 1 if '#' not in springs else 0
+    t = 0
+    if try_pattern(f"{'#' * damaged_groups[0]}", springs) == True:
+        # print(springs, springs[damaged_groups[0] + 1:])
+        t += guess(springs[damaged_groups[0] + 1:], damaged_groups[1:])
+        # r = guess(springs[1:], damaged_groups)
+        # return t + r
+    # print("T", t)
+    if springs[0] == '#':
+        return t
+    return t + guess(springs[1:], damaged_groups)
+
+
+def guess2(springs, damaged_groups):
     i = springs.find("?")
     if i != -1:
-        print(springs)
-        return guess(f"{springs[:i]}#{springs[i + 1:]}", damaged_groups) + guess(f"{springs[:i]}.{springs[i + 1:]}", damaged_groups)
+        # print(springs)
+        return guess2(f"{springs[:i]}#{springs[i + 1:]}", damaged_groups) + guess2(f"{springs[:i]}.{springs[i + 1:]}", damaged_groups)
     # print(springs, damaged_groups, count_groups(springs))
     return count_groups(springs) == damaged_groups
 
@@ -28,21 +85,29 @@ def guess(springs, damaged_groups):
 def get_new_springs(springs: str, damaged_groups: str):
     return f"{springs}?{springs}", f"{damaged_groups},{damaged_groups}"
 
+def get_new_springs5(springs: str, damaged_groups: str):
+    return f"{springs}?{springs}?{springs}?{springs}?{springs}", f"{damaged_groups},{damaged_groups},{damaged_groups},{damaged_groups},{damaged_groups}"
 
 def main():
     with open("input.txt") as file:
-        lines = List#sorted(file.readlines(), key=lambda x: x.count("?"), reverse=True)
+        lines = sorted(file.read().split('\n')[:-1], key=lambda x: x.count("?"), reverse=False)
 
     sum = 0
-    start = time.time()
     for i, line in enumerate(lines):
-        springs, damaged_groups = line.split()
-        first_pattern = guess(springs, list(map(int, damaged_groups.split(","))))
-        springs, damaged_groups = get_new_springs(*line.split())
-        second_pattern = guess(springs, list(map(int, damaged_groups.split(","))))
-        sum += first_pattern * pow(second_pattern // first_pattern, 4)
-        end = time.time() - start
-        # print(f"{i + 1} {len(lines)} -- {int(end // 3600)}:{int(end // 60)}:{end % 60:.2f} -- {springs}")
+        print(i, line)
+        # springs, damaged_groups = line.split()
+        # first_pattern = guess(springs, list(map(int, damaged_groups.split(","))))
+        # springs, damaged_groups = get_new_springs(*line.split())
+        # ttt = guess2(springs, list(map(int, damaged_groups.split(","))))
+        # second_pattern = guess(springs, list(map(int, damaged_groups.split(","))))
+        springs, damaged_groups = get_new_springs5(*line.split())
+        # if first_pattern * pow(second_pattern // first_pattern, 4) != guess(springs, list(map(int, damaged_groups.split(",")))):
+            # print(springs, damaged_groups)
+        # if ttt != second_pattern:
+        #     print(ttt, second_pattern, springs, damaged_groups)
+        # print()
+        # sum += first_pattern * pow(second_pattern // first_pattern, 4)
+        sum += guess(springs, list(map(int, damaged_groups.split(","))))
     print(sum)
 
 
